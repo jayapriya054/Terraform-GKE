f3b91f761c7d5b90cf4e4d14af01b4f8cf3f583a
Jayapriya Muthuramasamy <63770922+jayapriya054@users.noreply.github.com>
2024-05-20 19:26:00 -0400

    Update main.tf

--- modules/app/service/main.tf
+++ modules/app/service/main.tf
@@ -1,1 +1,20 @@
+resource "kubernetes_service" "app_service" {
+  metadata {
+    namespace = var.namespace
+    name      = var.app
+  }
 
+  spec {
+    type = var.lb
+    selector = {
+      app = var.myapp
+    }
+
+
+    port {
+      port        = var.port
+      target_port = var.targetPort
+    }
+  }
+
+}
