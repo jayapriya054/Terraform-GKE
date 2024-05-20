be7ef01d9e8db67004d14cb2dc4f65f8df91cba1
Jayapriya Muthuramasamy <63770922+jayapriya054@users.noreply.github.com>
2024-05-20 19:23:18 -0400

    Update main.tf

--- modules/app/deployments/main.tf
+++ modules/app/deployments/main.tf
@@ -1,1 +1,43 @@
 
+resource "kubernetes_deployment" "app_deployment" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+    
+    labels = {
+      app = var.myapp
+      env = var.environment
+    }
+  }
+
+  spec {
+    replicas = var.replica
+
+    selector {
+      match_labels = {
+        app = var.myapp
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.myapp
+        }
+      }
+
+      spec {
+        container {
+          image = var.image
+          name = var.myapp
+          port{
+             container_port = var.port
+              }
+	  
+          
+        }
+      }
+    }
+  }
+}
+
